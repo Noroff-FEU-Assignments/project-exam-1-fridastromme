79473866c8fa79f3c626b86fa8746af44e07af1f
Fridastromme <fridastromme@hotmail.com>
2021-05-22 18:56:41 +0200

    formatting

--- JS/modal.js
+++ JS/modal.js
@@ -1,40 +1,36 @@
-const modal = document.querySelector(".modal");
-const modalImage = document.querySelector(".modal__image");
-const content = document.querySelector(".content");
-const close = document.getElementsByClassName("close")[0];
+const modal = document.querySelector ('.modal');
+const modalImage = document.querySelector ('.modal__image');
+const content = document.querySelector ('.content');
+const close = document.getElementsByClassName ('close')[0];
 
+async function openModal () {
+  const response = await fetch (url);
+  const details = await response.json ();
 
-async function openModal() {
+  console.log (details);
 
-    const response = await fetch(url);
-    const details = await response.json();
+  const featuredImage =
+    details._embedded['wp:featuredmedia'][0].media_details.sizes.full
+      .source_url;
+  const title = details.title.rendered;
 
-    console.log(details);
+  storyImage.onclick = function () {
+    modal.style.display = 'inline-block';
+    modalImage.src = featuredImage;
+    modalImage.alt = title;
+  };
 
-    const featuredImage = details._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
-    const title = details.title.rendered;
-
-
-
-    storyImage.onclick = function() {
-        modal.style.display = "inline-block";
-        modalImage.src = featuredImage;
-        modalImage.alt = title;
-    }
-
-
-    close.onclick = function() {
-        modal.style.display = "none";
-    }
+  close.onclick = function () {
+    modal.style.display = 'none';
+  };
 }
 
+openModal ();
 
-openModal();
-
-function closeModal(event) {
-    if (event.target == storyImage) {
-        modal.style.display = "none";
-    }
+function closeModal (event) {
+  if (event.target == storyImage) {
+    modal.style.display = 'none';
+  }
 }
 
-content.addEventListener("click", closeModal);
+content.addEventListener ('click', closeModal);
